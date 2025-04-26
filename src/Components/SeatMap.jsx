38263ac79b775015e12a73c74dea36d85599183d
khalid saif <k1s1saif@gmail.com>
2025-04-26 18:27:27 +0800

    Refactor SeatMap component layout and improve accessibility features

--- src/Components/SeatMap.jsx
+++ src/Components/SeatMap.jsx
@@ -27,88 +27,105 @@ function SeatMap() {
 
     return (
         <>
-            <div>
+            <div className='p-2  bg-gray-100 rounded-lg shadow-md  mt-4 mb-4 mx-auto max-w-screen-lg">'>
+
+
+
                 {seatMap ? (
                     <div className="text-center">
-                        <p className='text-3xl font-bold'>Aircraft: {seatMap.aircraft}</p>
-                        <p className='text-2xl font-bold'>Flight Number: {seatMap.flightNumber}</p>
-                        <p className='text-2xl font-bold'>Departure: {seatMap.departure}</p>
+                        <p className='text-2xl font-bold'>Boeing {seatMap.aircraft}</p>
+                        <p className='text-1xl font-bold'>{seatMap?.cabins[0].deck} Deck </p>
+
+                        <p className='text-center text-green-500 font-bold'>Choose your seat and enjoy the journy</p>
                     </div>
                 ) : (
                     <p>Loading seat map...</p>
                 )}
 
-                <br></br>
-            </div>
-            <div className="grid grid-cols-4  ">
-                <div className="col-span-1">
-                    <div className="flex flex-col justify-center items-center">
-                        <h1 className='text-3xl font-bold'>Legend</h1>
+
+
+                <div className="grid grid-cols-4 ">
+                    <div className="col-span-1 bg-gray-200 rounded-lg shadow-md">
+
+
+                        <div className='m-2 p-2 box-border  bg-gray-200 rounded-lg shadow-md m-4">'>
+                            <div >
+                                <p className='text-2xl font-bold'> Passenger</p>
+                                <p>Name :Mohmmad Khalid Saifullah</p>
+                            </div>
+
+                        </div>
+                        <div className='m-2 p-2 box-border  bg-gray-200 rounded-lg shadow-md '>
+                            <div className="">
+                                <h1 className='text-3xl  font-bold'>Legend</h1>
+                            </div>
+                            <div className="">
+                                <div className="flex items-center justify-start mb-2">
+                                    <div className="w-4 h-4 bg-gray-400 rounded-full mr-2"></div>
+                                    Available &nbsp;  &nbsp;
+                                    <div
+                                        className={`p-1  bg-blue-200 rounded-xl cursor-pointer flex justify-center items-center`}
+                                        style={{ width: '60px', margin: '0 auto' }}
+
+                                    >
+                                        <FaChair size={40} color='black' />
+                                    </div>
+
+                                </div>
+                                <div className="  flex items-center justify-start mb-2">
+                                    <div className="w-4 h-4 bg-gray-400 rounded-full mr-2"></div>
+                                    UnAvailable
+                                    <div
+                                        className={`p-1  bg-gray-300 rounded-xl cursor-pointer flex justify-center items-center`}
+                                        style={{ width: '60px', margin: '0 auto' }}
+
+                                    >
+                                        <FaChair size={40} color='darkgray' />
+                                    </div>
+
+
+                                </div>
+                                <div className="flex items-center">
+                                    <div className="w-4 h-4 bg-gray-400 rounded-full mr-2"></div>
+                                    Selected &nbsp;  &nbsp; &nbsp;
+                                    <div
+                                        className={`p-1  bg-green-200 rounded-xl cursor-pointer flex justify-center items-center`}
+                                        style={{ width: '60px', margin: '0 auto' }}
+
+                                    >
+                                        <FaChair size={40} color='black' />
+                                    </div>
+
+                                </div>
+
+                            </div>
+                        </div>
                     </div>
-                    <div className="flex flex-col justify-center items-center ">
-                        <div className="flex items-center justify-start mb-2">
-                            <div className="w-4 h-4 bg-gray-400 rounded-full mr-2"></div>
-                            &nbsp; &nbsp; Available &nbsp;
-                            <div
-                                className={`p-1  bg-blue-200 rounded-xl cursor-pointer flex justify-center items-center`}
-                                style={{ width: '60px', margin: '0 auto' }}
+                    <div className=" col-span-2 bg-gray-200 rounded-lg shadow-md p-4">
 
-                            >
-                                <FaChair size={40} color='black' />
-                            </div>
+                        <div className="grid grid-cols-9 gap-2 ">
 
+                            {seatRows && seatRows.map((row, index) => (
+                                row.seats.map((seat, seatIndex) => (
+
+                                    seat.available ? (
+                                        <Seat
+                                            key={`${index}-${seatIndex}`}
+                                            seat={seat}
+
+                                        />
+                                    ) : (<p></p>)
+                                ))
+                            ))}
                         </div>
-                        <div className="  flex items-center justify-start mb-2">
-                            <div className="w-4 h-4 bg-gray-400 rounded-full mr-2"></div>
-                            UnAvailable &nbsp;
-                            <div
-                                className={`p-1  bg-gray-200 rounded-xl cursor-pointer flex justify-center items-center`}
-                                style={{ width: '60px', margin: '0 auto' }}
-
-                            >
-                                <FaChair size={40} color='darkgray' />
-                            </div>
-
-
-                        </div>
-                        <div className="flex items-center">
-                            <div className="w-4 h-4 bg-gray-400 rounded-full mr-2"></div>
-                            &nbsp;&nbsp;&nbsp; &nbsp; Selected &nbsp;
-                            <div
-                                className={`p-1  bg-green-200 rounded-xl cursor-pointer flex justify-center items-center`}
-                                style={{ width: '60px', margin: '0 auto' }}
-
-                            >
-                                <FaChair size={40} color='black' />
-                            </div>
-
-                        </div>
+                    </div>
+                    <div className="col-span-1 bg-gray-200 rounded-lg shadow-md p-4">
 
                     </div>
+
                 </div>
-                <div className=" col-span-2">
-                    <p className='text-center text-green-500 font-bold'>Choose your seat and enjoy the journy</p>
-                    <div className="grid grid-cols-9 gap-2 ">
-
-                        {seatRows && seatRows.map((row, index) => (
-                            row.seats.map((seat, seatIndex) => (
-
-                                seat.available ? (
-                                    <Seat
-                                        key={`${index}-${seatIndex}`}
-                                        seat={seat}
-
-                                    />
-                                ) : (<p></p>)
-                            ))
-                        ))}
-                    </div>
-                </div>
-                <div className="col-span-1"></div>
 
             </div>
-
-
         </>
     )
 }
